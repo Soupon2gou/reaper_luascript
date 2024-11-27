98b63fe4eb2b85a597c51a96813780467ad3ff93
Soupon2gou <Soupon2gou@gmail.com>
2024-11-28 00:55:55 +0900

    python to lua

--- Stretch selected notes to next note.lua
+++ Stretch selected notes to next note.lua
@@ -1,38 +1,47 @@
-import reaper_python as RPR
-
-def stretch_notes_to_next_note():
-    take = RPR.MIDIEditor_GetTake(RPR.MIDIEditor_GetActive())
-    if not take:
-        RPR.ShowMessageBox("MIDIエディタが開かれていません。", "エラー", 0)
+-- 選択したMIDIノートを次のノートの開始位置まで引き伸ばす
+function stretch_notes_to_next()
+    -- アクティブなMIDIエディタのテイクを取得
+    local take = reaper.MIDIEditor_GetTake(reaper.MIDIEditor_GetActive())
+    if not take then
+        reaper.ShowMessageBox("MIDIエディタが開かれていません。", "エラー", 0)
         return
-    
-    # MIDIノート数を取得
-    _, note_count, _, _ = RPR.MIDI_CountEvts(take)
-    
-    if note_count == 0:
-        RPR.ShowMessageBox("選択されたMIDIノートがありません。", "エラー", 0)
+    end
+
+    -- ノート情報を収集
+    local notes = {}
+    local note_idx = -1
+    while true do
+        note_idx = reaper.MIDI_EnumSelNotes(take, note_idx)
+        if note_idx == -1 then break end
+        local _, _, _, start_ppq, end_ppq, _, _, _ = reaper.MIDI_GetNote(take, note_idx)
+        table.insert(notes, {index = note_idx, start = start_ppq, ["end"] = end_ppq})
+    end
+
+    -- ノートが少なくとも1つ必要
+    if #notes < 1 then
+        reaper.ShowMessageBox("選択されたノートがありません。", "エラー", 0)
         return
+    end
 
-    # ノート情報を取得
-    notes = []
-    for i in range(note_count):
-        _, selected, muted, start_ppq, end_ppq, chan, pitch, vel = RPR.MIDI_GetNote(take, i)
-        if selected:
-            notes.append((i, start_ppq, end_ppq, pitch))
+    -- ノートを次のノートの開始位置まで伸ばす
+    for i = 1, #notes - 1 do
+        local current_note = notes[i]
+        local next_note = notes[i + 1]
+        reaper.MIDI_SetNote(
+            take,
+            current_note.index,
+            nil, nil,
+            current_note.start,
+            next_note.start,  -- 次のノートの開始位置まで伸ばす
+            nil, nil, nil, true
+        )
+    end
 
-    # ノートを次のノートの開始位置まで引き伸ばす
-    for idx, note in enumerate(notes):
-        note_idx, start_ppq, end_ppq, pitch = note
-        
-        # 次のノートが存在するか確認
-        if idx + 1 < len(notes):
-            next_note_start = notes[idx + 1][1]  # 次のノートの開始位置
-            # ノートを更新
-            RPR.MIDI_SetNote(take, note_idx, True, False, start_ppq, next_note_start, chan, pitch, vel, False)
+    -- MIDIデータをソートして変更を適用
+    reaper.MIDI_Sort(take)
+end
 
-    # 変更を適用
-    RPR.MIDI_Sort(take)
-    RPR.ShowMessageBox("ノートを次のノートまで引き伸ばしました。", "完了", 0)
-
-# 実行
-stretch_notes_to_next_note()
+-- メイン処理の実行
+reaper.Undo_BeginBlock()
+stretch_notes_to_next()
+reaper.Undo_EndBlock("Stretch selected notes to next note", -1)
